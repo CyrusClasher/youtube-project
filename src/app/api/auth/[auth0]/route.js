5f4cf6a324f9d841e245e69fc6fda63f6597a2fa
CyrusClasher <cyrusclasher08@gmail.com>
2025-01-12 16:08:37 +0530

    removed some bugs

--- src/app/api/auth/[auth0]/route.js
+++ src/app/api/auth/[auth0]/route.js
@@ -1,13 +1,14 @@
 // app/api/auth/[auth0]/route.js
 import { handleAuth } from "@auth0/nextjs-auth0";
-
-export const GET = handleAuth({
-  async login(req, res) {
-    await handleLogin(req, res, {
-      authorizationParams: {
-        audience: "your-api-identifier", // Add your Auth0 API identifier
-        scope: "openid profile email",
-      },
-    });
-  },
-});
+export const GET = handleAuth();
+export const POST = handleAuth();
+// export const GET = handleAuth({
+//   async login(req, res) {
+//     await handleLogin(req, res, {
+//       authorizationParams: {
+//         audience: "your-api-identifier", // Add your Auth0 API identifier
+//         scope: "openid profile email",
+//       },
+//     });
+//   },
+// });
